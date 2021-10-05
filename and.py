93e974d1668e791e9f76e7f8d157cd6fe12b58cf
Neeraj Kumar43 <nkumar44@in.ibm.com>
2021-10-05 05:39:21 +0530

    docstring

--- and.py
+++ and.py
@@ -3,20 +3,25 @@ from utils.all_utils import prepare_data
 import pandas as pd
 import numpy as np
 
-AND = {
-    'X1' : [0, 0, 1, 1],
-    'X2' : [0, 1, 0, 1],
-    'y'  : [0, 0, 0, 1]
-}
+def main():
+    AND = {
+        'X1' : [0, 0, 1, 1],
+        'X2' : [0, 1, 0, 1],
+        'y'  : [0, 0, 0, 1]
+    }
 
-df = pd.DataFrame(AND)
-df
+    df = pd.DataFrame(AND)
+    df
 
-X, Y = prepare_data(df)
-ETA = 0.3 
-EPOCHS = 10
+    X, Y = prepare_data(df)
+    ETA = 0.3 
+    EPOCHS = 10
 
-model = Perceptron(eta = ETA, epochs = EPOCHS)
-model.fit(X, Y)
+    model = Perceptron(eta = ETA, epochs = EPOCHS)
+    model.fit(X, Y)
 
-_ = model.total_loss()
+    _ = model.total_loss()
+
+if __name__ == '__main__':
+    main()
+    
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -3,6 +3,14 @@
 
 
 def prepare_data(df):
+  """This is to separate the dependent and independent features
+
+  Args:
+      df (pd.DataFrame): its the pandas DataFrame   
+
+  Returns:
+      tuple: it returns the tuples of dependent and independednt features
+  """
   X = df.drop('y', axis = 1)
   y = df['y']
   return X, y
